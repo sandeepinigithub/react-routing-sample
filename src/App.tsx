a8359964dd3e3d976ffb68aaf0509a3d42b79085
Sandeep Kumar Shukla <sandeepini.2012@gmail.com>
2022-04-13 11:13:25 +0530

    redirect path

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import {Route,Routes} from 'react-router-dom'
+import {Navigate, Route,Routes} from 'react-router-dom'
 import HeaderComponent from './components/HeaderComponent/HeaderComponent'
 import HomeComponent from './components/HomeComponent/HomeComponent';
 import FeaturesComponent from './components/FeaturesComponent/FeaturesComponent';
@@ -14,10 +14,12 @@ function App() {
     <HeaderComponent/>
     <main>
       <Routes>
+        <Route path='/' element={<HomeComponent/>}/>
         <Route path='/home' element={<HomeComponent/>}/>
         <Route path='/features' element={<FeaturesComponent/>}/>
         <Route path='/about' element={<AboutComponent/>}/>
         <Route path='/about/:name' element={<AboutComponent/>}/>
+        <Route path='/*' element={<Navigate to="/" replace />}/>
       </Routes>
     </main>
     </>
